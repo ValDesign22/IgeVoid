350660addac037a26b9be0d8fd8100600350ee07
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-04-03 14:52:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const fs = require("fs");
-
 const Discord = require("discord.js");
+
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+
+const { Player } = require('discord-player');
+
+client.player = new Player(client);
 
 const config = require("./config.json");
 client.config = config;
+
+const { GiveawaysManager } = require('discord-giveaways');
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} est entré dans le giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} est sortie du giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
 
 fs.readdir("./events/", (_err, files) => {
     files.forEach((file) => {
@@ -17,6 +40,14 @@ fs.readdir("./events/", (_err, files) => {
     });
     console.log(`${files.length} events`)
 });
+
+const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+
+for (const file of player) {
+    console.log(`Loading discord.js event ${file}`);
+    const event = require(`./player/${file}`);
+    client.player.on(file.split(".")[0], event.bind(null, client));
+};
 
 client.commands = new Discord.Collection();
 
@@ -32,3 +63,26 @@ fs.readdir("./commands/", (_err, files) => {
 });
 
 client.login(client.config.token)
+
+/* Dashboard */
+
+const http = require("http");
+const settings = require("./settings.json");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render('index', { bot: settings.website })
+})
+
+app.get("/commands", (req, res) => {
+  res.render("commands", {bot: settings.website, commands: settings.commands })
+})
+
+const listener = server.listen(client.config.port, function() {
+    console.log("Dashboard en ligne sur le port " + listener.address().port);
+})
