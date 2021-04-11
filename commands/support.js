09dbf1600d2389d39d4956ea8518e477b13abe29
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-04-11 14:56:28 +0200

    Delete commands directory

--- commands/support.js
+++
@@ -1,9 +0,0 @@
-const { MessageEmbed } = require("discord.js");
-
-exports.run = (client, message) => {
-    const e = new MessageEmbed()
-    .setTitle(`Serveur Support De ${client.user.username}`)
-    .setURL("https://discord.gg/nDKqMN6cG8")
-    
-    message.channel.send(e)
-}
