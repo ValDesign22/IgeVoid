ecb4a137d63ee19c72728d4118bddfd2925399fb
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-04-11 15:01:17 +0200

    Delete events directory

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = (client) => {
-    console.log(`${client.user.tag} est en ligne.`)
-
-    client.user.setActivity(`${client.config.prefix}help | Dans ${client.guilds.cache.size} serveurs | ${client.users.cache.size} utilisateurs`, {type: "WATCHING"})
-}
