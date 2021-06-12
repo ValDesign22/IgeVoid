62ffbf424fc953fb7be26a57090d2d763c34a139
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-06-12 15:33:11 +0200

    Delete events directory

--- events/guildCreate.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, guild) => {
-    const serveur = client.channels.cache.find(c => c.id === `${client.configs.addChannel}`);
-    
-    serveur.send(`${client.emotes.join} Nouveau serveur rejoint:\n${guild.name}\n\nObjectif: ${client.guilds.cache.size}/50`);
-
-    client.ranks.set(`lvl_${guild.id}`, "off");
-}
