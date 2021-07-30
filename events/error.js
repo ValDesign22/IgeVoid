47be219748a8f7508ef424f6e0f2c60e4bcbaa56
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-07-30 18:04:07 +0200

    Delete events directory

--- events/error.js
+++
@@ -1,6 +0,0 @@
-const moment = require("moment");
-
-module.exports = (client, error) => {
-    console.log(error.message);
-    client.channels.cache.get(client.configs.logsChannel).send(`[${moment(new Date()).locale("fr").format('lll')}] [ERROR] \`\`\`js\n${error.message}\n\`\`\``)
-}
--- events/newUser.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client, user) => {
-    if (client.dash.has(`${user.id}`)) return;
-    console.log(`${user.username} just logged into the dashboard`);
-    client.dash.set(`${user.id}`, true)
-}
